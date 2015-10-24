255bdb5c5c2ec99b0fc47e2c36a44554badd8e99
Vish Yella <Visu99@yahoo.com>
2015-10-24 18:59:22 -0400

    submission 

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 10/23/2015 Author: Vish Yella
+## This file contains source code for two functions makeCacheMatrix and cacheSolve
+##
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##      If the inverse has already been calculated (and the matrix has not changed),
+##      then cacheSolve retrieve the inverse from the cache.
+## 
+## These functions demonstrate the features of below operators
+##      assignment operator: <-
+##      superassignment operator: <<-
+ 
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(varMatrix = matrix()) {
+        varInv <- NULL
+        set <- function(argMatrix){  
+                varMatrix <<- argMatrix # set the data varMatrix
+                varInv    <<- NULL      # not specified yet so initialize it
+        }
+        get <-function() varMatrix  # just return data varMatrix
+        
 
-makeCacheMatrix <- function(x = matrix()) {
-
+        setinverse <- function(argInv) varInv <<- argInv
+        getinverse <- function() varInv
+        
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##      If the inverse has already been calculated (and the matrix has not changed),
+##      then cacheSolve retrieve the inverse from the cache.
+cacheSolve <- function(argMatrix, ...) {
+         
+        varInv <- argMatrix$getinverse()  # get inverse that's already there within matrix object 
+        if(!is.null(varInv)){             # if it is already computed, just return that
+                message("getting cached data")
+                return(varInv)
+        }
+        # inverse is not already computed, so compute now
+        matrixData <- argMatrix$get()
+        varInv <- solve(matrixData,...)
+        argMatrix$setinverse(varInv)  # assign the inverse to given matrix object
+        varInv # return the freshly computed inverse
 }
